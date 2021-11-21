aea1bd29dbd68abd44c6b9ab90e38c824c0d1456
Habib Royni Mujtahid <habib.sickh@gmail.com>
2021-11-21 22:07:32 +0700

    init project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import ToggleColorMode from '../components/ToggleColorMode'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ToggleColorMode />
+      <Component {...pageProps} />
+    </ChakraProvider>
+    )
 }
 
 export default MyApp
